2e4a762046d799c091cfd32b95b7a010c6367373
SatyaPrakashPalai <sp8368755@gmail.com>
2023-06-12 08:49:07 +0530

    fix: removed the firebase api config from the frontend

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,19 @@
-
-import 'firebase/firestore';
-import {initializeApp} from "firebase/app"
-import { getFirestore } from 'firebase/firestore';
+import "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import dotenv from "dotenv";
 
-
-
+dotenv.config(); // Load environment variables from .env file
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAF7e_L0857Wp8XrtKGaOFgtnpj2WP5OEg",
-  authDomain: "tinder-clone-2d336.firebaseapp.com",
-  projectId: "tinder-clone-2d336",
-  storageBucket: "tinder-clone-2d336.appspot.com",
-  messagingSenderId: "1004897285432",
-  appId: "1:1004897285432:web:5eaf9a6c77782cf436fdcd",
-  measurementId: "G-892KSPBLC3"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -23,7 +22,5 @@ const provider = new GoogleAuthProvider();
 
 const auth = getAuth(app);
 
-
-export {provider, auth};
+export { provider, auth };
 export default db;
-
